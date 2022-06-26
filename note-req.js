787f0408e93f25565f2475cbec79a1f8d7eeb15b
Trần Trọng Hòa <86024288+hexzzz2008@users.noreply.github.com>
2022-06-26 21:30:04 +0700

    Create note-req.js

---
+++ note-req.js
@@ -0,0 +1,30 @@
+var request = require('request');
+
+var headers = {
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+    'Accept-Language': 'en-US,en;q=0.9',
+    'Cache-Control': 'max-age=0',
+    'Connection': 'keep-alive',
+    'Content-Type': 'application/x-www-form-urlencoded',
+    'Origin': 'http://viewyt.xyz',
+    'Referer': 'http://viewyt.xyz/index.php',
+    'Upgrade-Insecure-Requests': '1',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.53 Safari/537.36 Edg/103.0.1264.37'
+};
+
+var dataString = 'text_or_id_page=demo+t%E1%BA%A1o+note&action=create';
+
+var options = {
+    url: 'http://viewyt.xyz/api/taonote.php',
+    method: 'POST',
+    headers: headers,
+    body: dataString
+};
+
+function callback(error, response, body) {
+    if (!error && response.statusCode == 200) {
+        console.log(body);
+    }
+}
+
+request(options, callback);
